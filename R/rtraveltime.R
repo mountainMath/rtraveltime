48b0c02c19c17b2532ae94e80c4236c5ba9c6732
Jens von Bergmann <jens@mountainmath.ca>
2019-11-05 10:08:36 -0800

    allow for arrival time isochrones

--- R/rtraveltime.R
+++ R/rtraveltime.R
@@ -8,6 +8,34 @@
 #' @return a vector of type sf containging the isochorone polygons
 #' @export
 get_departure_isochrone <- function(data){
+  get_isochrone(data,"departure")
+}
+#' Get isochrone based on arrival times and locations
+#' Expects `rtraveltime.id` and `rtraveltime.key` options to be set with traveltime API information
+#'
+#' @param data tibble of type sf with point geometry for starting locations and fields
+#'   `travel_time` travel time in seconds
+#'   `mode_type` transportation type
+#'   `id` (optional), must be unique if present the return data will be labeled by `id` values
+#' @return a vector of type sf containging the isochorone polygons
+#' @export
+get_arrival_isochrone <- function(data){
+  get_isochrone(data,"departure")
+}
+
+
+#' Get isochrone based on departure or arrival times and locations
+#' Expects `rtraveltime.id` and `rtraveltime.key` options to be set with traveltime API information
+#'
+#' @param data tibble of type sf with point geometry for starting locations and fields
+#'   `travel_time` travel time in seconds
+#'   `mode_type` transportation type
+#'   `id` (optional), must be unique if present the return data will be labeled by `id` values
+#' @param type either 'departure' or 'arrival' to indicate the type of time request
+#' @return a vector of type sf containging the isochorone polygons
+#' @export
+get_isochrone <- function(data,type=c("departure","arrival")){
+  if (length(type)!=1 || !(type %in% c("departure","arrival"))) stop(paste0("Invalid type ",type))
   iso_time_string <- function(x){
     strftime(as.POSIXlt(x,"UTC"),format="%Y-%m-%dT%H:%M:00Z")
   }
@@ -15,7 +43,7 @@ get_departure_isochrone <- function(data){
                "X-Api-Key"=getOption("rtraveltime.key"),
                "Accept"="application/vnd.wkt+json",
                "Content-Type"="application/json")
-  url="api.traveltimeapp.com/v4/time-map"
+  url="http://api.traveltimeapp.com/v4/time-map"
 
   if (!("id" %in% names(data))) {
     d <- data %>% dplyr::mutate(id=row_number())
@@ -24,15 +52,15 @@ get_departure_isochrone <- function(data){
   }
 
   payload <- d %>%
-    dplyr::bind_cols(sf::st_coordinates(sf::st_sf(.)) %>% tibble::as.tibble()) %>%
+    dplyr::bind_cols(sf::st_coordinates(sf::st_sf(.)) %>% tibble::as_tibble()) %>%
     dplyr::mutate(payload=paste0('{"id": "',.data$id,
                                  '", "coords": {"lat": ',.data$Y,',  "lng": ',.data$X,
                                  '}, "transportation": {"type": "',.data$mode_type,
-                                 '"}, "departure_time": "',iso_time_string(.data$departure_time),
+                                 '"}, "',type,'_time": "',iso_time_string(.data$departure_time),
                                  '","travel_time": ',.data$travel_time,'}')) %>%
     pull("payload") %>%
     paste0(collapse = ", ") %>%
-    paste0('{"departure_searches": [',.,']}')
+    paste0('{"',type,'_searches": [',.,']}')
 
   response <- httr::POST(url,config=httr::add_headers(headers),body=payload)
   result <- NULL
@@ -44,6 +72,8 @@ get_departure_isochrone <- function(data){
     if (!("id" %in% names(data))) result <- results %>% dplyr::select(-id)
   } else {
     warning(paste0("Request failed with status code ",response$status_code))
+    warning(httr::content(response)$description)
+    warning(httr::content(response)$additional_info %>% unlist %>% paste0(collapse = "\n"))
   }
   result
 }
